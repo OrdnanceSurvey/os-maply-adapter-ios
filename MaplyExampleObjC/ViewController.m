c98b22ce8ea57a6a0931c3f78bea3d5c2cf4ad7c
drhaynes <drhmbp@gmail.com>
2016-03-19 23:34:26 +0000

    Initial map creation

--- MaplyExampleObjC/ViewController.m
+++ MaplyExampleObjC/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+@import MaplyAdapter;
+#import "MaplyComponent.h"
 
-@interface ViewController ()
+@interface ViewController ()<MaplyViewControllerDelegate>
+
+@property (nonatomic, strong) MaplyViewController *maplyViewController;
 
 @end
 
@@ -16,10 +20,48 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    // Create map and tile layer
+    self.maplyViewController = [self createMaplyViewController];
+    [self addMapToViewHierarchy];
+    MaplyQuadImageTilesLayer *tileLayer = [OSTileSourceFactory create27700TileLayer];
+    [self.maplyViewController addLayer:tileLayer];
+
+    // Create test marker
+    MaplyCoordinate londonCoordinate = MaplyCoordinateMakeWithDegrees(-0.1275, 51.507222);
+    MaplyScreenMarker *marker = [[MaplyScreenMarker alloc] init];
+    marker.image = [UIImage imageNamed:@"map_pin"];
+    marker.loc = londonCoordinate;
+    marker.size = CGSizeMake(40, 40);
+    marker.selectable = true;
+    marker.userObject = @"London";
+    [self.maplyViewController addScreenMarkers:@[ marker ] desc:nil mode:MaplyThreadAny];
+
+    // Set initial map position to the centre of GB
+    MaplyCoordinate centreOfGB = MaplyCoordinateMakeWithDegrees(-2.42, 54.83);
+    MaplyCoordinate centreOfGBLocal = [self.maplyViewController.coordSys geoToLocal:centreOfGB];
+    [self.maplyViewController setPosition:centreOfGBLocal height:1.7];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+- (MaplyViewController *)createMaplyViewController {
+    MaplyViewController *maplyViewController = [[MaplyViewController alloc] initWithMapType:MaplyMapTypeFlat];
+    maplyViewController.coordSys = [OSBNGUtils buildBritishNationalGrid];
+    maplyViewController.viewWrap = false;
+    maplyViewController.doubleTapZoomGesture = true;
+    maplyViewController.twoFingerTapGesture = true;
+    maplyViewController.cancelAnimationOnTouch = true;
+    maplyViewController.delegate = self;
+    return maplyViewController;
+}
+
+- (void)addMapToViewHierarchy {
+    [self.view addSubview:self.maplyViewController.view];
+    [self.view sendSubviewToBack:self.maplyViewController.view];
+    [self addChildViewController:self.maplyViewController];
 }
 
 @end
