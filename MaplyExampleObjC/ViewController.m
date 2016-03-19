acf7483960324a12b883e1d38ea19cc9a44da49c
drhaynes <drhmbp@gmail.com>
2016-03-19 23:11:45 +0000

    Create Obj-c example app target

---
+++ MaplyExampleObjC/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  MaplyExampleObjC
+//
+//  Created by David Haynes on 19/03/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
