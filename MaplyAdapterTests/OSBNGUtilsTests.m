4f61bacaa8e94c08190be7a2298c098098099bc1
drhaynes <drhmbp@gmail.com>
2016-03-19 21:26:24 +0000

    Add tilesource creation tests

--- MaplyAdapterTests/OSBNGUtilsTests.m
+++ MaplyAdapterTests/OSBNGUtilsTests.m
@@ -9,6 +9,7 @@
 #import <XCTest/XCTest.h>
 @import MaplyAdapter;
 @import Expecta;
+#import "MaplyComponent.h"
 
 @interface OSBNGUtilsTests : XCTestCase
 
@@ -16,9 +17,24 @@
 
 @implementation OSBNGUtilsTests
 
-- (void)test {
+- (void)testStandardBNGCoordinateSystemCreation {
     MaplyCoordinateSystem *coordinateSystem = [OSBNGUtils buildBritishNationalGrid];
     expect(coordinateSystem).toNot.beNil();
+    MaplyBoundingBox bounds = [coordinateSystem getBounds];
+    expect(bounds.ll.x).to.beCloseToWithin(1393.019653320312, FLT_EPSILON);
+    expect(bounds.ll.y).to.beCloseToWithin(13494.9765625, FLT_EPSILON);
+    expect(bounds.ur.x).to.beCloseToWithin(671196.375, FLT_EPSILON);
+    expect(bounds.ur.y).to.beCloseToWithin(1230275.0, FLT_EPSILON);
+}
+
+- (void)testTilesourceBNGCoordinateSystemCreation {
+    MaplyCoordinateSystem *coordinateSystem = [OSBNGUtils bngForTileSource];
+    expect(coordinateSystem).toNot.beNil();
+    MaplyBoundingBox bounds = [coordinateSystem getBounds];
+    expect(bounds.ll.x).to.beCloseToWithin(-238375.0, FLT_EPSILON);
+    expect(bounds.ll.y).to.beCloseToWithin(-458752.0, FLT_EPSILON);
+    expect(bounds.ur.x).to.beCloseToWithin(1596633.0, FLT_EPSILON);
+    expect(bounds.ur.y).to.beCloseToWithin(1376256.0, FLT_EPSILON);
 }
 
 @end
